76080fb730e7e8eef01779ff9a26fd8637861183
AlvaroSanchezM <101331961+AlvaroSanchezM@users.noreply.github.com>
2024-04-23 01:14:53 +0200

    Complete scheduling.py

--- scheduling.py
+++ scheduling.py
@@ -44,7 +44,53 @@ class Scenario:
     def maxCI_MinRate(self):
         r_final = []
         # TODO: To Complete
-        return r_final
+         #Primero calcular users x (r_sla_Mbps / bps_per_hz) total para repartir la banda en un inicio
+        requerido = 0
+        for zone in self.zones:
+            sla_Mhz_per_user = zone.r_sla_mbps/zone.bps_per_hz
+            sla_Mhz_zone = zone.users * sla_Mhz_per_user
+            requerido += sla_Mhz_zone
+        print("Para dar a todos r_sla_mbps (igual para todos) se necesitan "+ str(requerido) + " Mbps")
+        # Si se necesita más banda para cumplir la SLA que la que hay, se reparte a partes iguales la velocidad disponible
+        if requerido > self.b_total_MHz:
+            tot_us_bps_hz=0
+            for zone in self.zones:
+                us_bps_hz = zone.users * zone.bps_per_hz
+                tot_us_bps_hz += us_bps_hz
+            rfinal_per_user=self.b_total_MHz/tot_us_bps_hz
+            for zone in self.zones:
+                zone_name = zone.name
+                r_user = rfinal_per_user
+                r_final.append([zone_name, r_user])
+            return r_final
+        #si hay la justa para cumplir la sla en todas las zonas, se reparte a partes iguales de nuevo
+        elif requerido == self.b_total_MHz:
+            for zone in self.zones:
+                zone_name = zone.name
+                r_user = zone.r_sla_mbps
+                r_final.append([zone_name, r_user])
+            return r_final
+        # Si hay de sobra para repartir un extra además de la usada para el SLA en todas las terminales,
+        # se reparte a los que mejor la puedan usar con max CI
+        else:
+            self.b_remain_MHz = self.b_total_MHz - requerido
+            aux_b_extra = self.b_remain_MHz
+            
+            for zone in self.zones:
+                aux_r_peak_mbps = zone.r_peak_mbps - zone.r_sla_mbps
+                zone_name = zone.name
+                #-aux_b_extra, r_user = self.computeZone(zone)-
+                n_users = zone.users
+                ro_bpsHz = zone.bps_per_hz
+                aux_b_extra = n_users * aux_r_peak_mbps / zone.bps_per_hz
+                if aux_b_extra >= self.b_remain_MHz:
+                    aux_b_extra = self.b_remain_MHz
+                b_user = aux_b_extra / n_users
+                r_user = b_user * ro_bpsHz + zone.r_sla_mbps
+                #------------------------------------------
+                self.b_remain_MHz -= aux_b_extra
+                r_final.append([zone_name, r_user])
+            return r_final
     
 def main():
     r_peak_mbps = 3
